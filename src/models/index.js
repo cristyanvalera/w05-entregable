0ed564b92c54defa1c9ff72139a5ad8cfb9b5995
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-04-28 21:46:23 -0400

    fix styles

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ const Movie = require("./Movie");
 Movie.belongsToMany(Actor, { through: 'movie_actor' });
 Actor.belongsToMany(Movie, { through: 'movie_actor' });
 
-Movie.belongsToMany(Director, {through: 'movie_director'});
+Movie.belongsToMany(Director, { through: 'movie_director' });
 Director.belongsToMany(Movie, { through: 'movie_director' });
 
 Movie.belongsToMany(Genre, { through: 'movie_genre' });
